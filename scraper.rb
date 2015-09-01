22f906f3bbf9c5789d50cda938ed19bd3ca4b3cd
Chris Dawson <xrdawson@gmail.com>
2015-09-01 18:51:01 -0400

    Use attr_accessor

--- scraper.rb
+++ scraper.rb
@@ -2,16 +2,19 @@ require 'mechanize'
 
 class Scraper
 
+  attr_accessor :root
+  attr_accessor :agent
+
   def initialize
     @root = "http://web.archive.org/web/20030820233527/http://bytravelers.com/journal/entry/"
-    @mechanize = Mechanize.new 
+    @agent = Mechanize.new 
   end
 
   def run
     100.times do |i|
       begin
         url = "#{@root}#{i}"
-        @mechanize.get( url ) do |page|
+        @agent.get( url ) do |page|
           puts "#{i} #{page.title}"
         end
       end
