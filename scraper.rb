798360ecc5f2bf3f6bbc05a9192aa4a647f37bac
Chris Dawson <xrdawson@gmail.com>
2015-09-02 16:26:02 -0400

    Add caching and refactor

--- scraper.rb
+++ scraper.rb
@@ -1,23 +1,46 @@
 require 'mechanize'
+require 'vcr'
+VCR.configure do |c|
+  c.cassette_library_dir = 'cached'
+  c.hook_into :webmock
+end
 
 class Scraper
 
   attr_accessor :root
   attr_accessor :agent
+  attr_accessor :pages
 
   def initialize
     @root = "http://web.archive.org/web/20030820233527/http://bytravelers.com/journal/entry/"
     @agent = Mechanize.new 
+    @pages = []
+  end
+
+  def scrape
+    100.times do |i|
+      begin
+        VCR.use_cassette("bt_#{i}") do
+          url = "#{@root}#{i}"
+          @agent.get( url ) do |page|
+            if page.title.eql? "Read Journal Entries"
+              pages << page
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def process_title( row )
+    puts row.text().strip
   end
 
   def run
-    100.times do |i|
-      begin
-        url = "#{@root}#{i}"
-        @agent.get( url ) do |page|
-          puts "#{i} #{page.title}"
-        end
-      end
+    scrape()
+    @pages.each do |page|
+      rows = ( page / "table[valign=top] tr" )
+      process_title( rows[0] )
     end
   end
 
