0d6d82c52a7690fc332a37b776a0931c1f4a32be
Chris Dawson <xrdawson@gmail.com>
2015-09-02 17:57:53 -0400

    Write scraper tests

--- scraper_spec.rb
+++ scraper_spec.rb
@@ -1,8 +1,18 @@
 require './scraper'
 
 describe "#run" do
-  it "should run" do
-    scraper = Scraper.new
-    scraper.run()
+  before :each  do
+    @scraper = Scraper.new
   end
+
+  describe "#process_titles" do
+    it "should correct titles with double quotes" do
+      expect( @scraper.process_title( ' something " with a double quote' ) ).to_not match( /"/ )
+    end
+    
+    it "should strip whitespace from titles" do
+      expect( @scraper.process_title( '\n\n something between newlines \n\n' ) ).to_not match( /^\n\n/ )
+    end
+  end
+
 end
