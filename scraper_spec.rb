57c0cef7058b008cf12cdc552140333975329018
Chris Dawson <xrdawson@gmail.com>
2015-09-03 15:29:22 -0400

    More readable tests

--- scraper_spec.rb
+++ scraper_spec.rb
@@ -7,11 +7,13 @@ describe "#run" do
 
   describe "#process_titles" do
     it "should correct titles with double quotes" do
-      expect( @scraper.process_title( ' something " with a double quote' ) ).to_not match( /"/ )
+      str = ' something " with a double quote' 
+      expect( @scraper.process_title( str ) ).to_not match( /"/ )
     end
     
     it "should strip whitespace from titles" do
-      expect( @scraper.process_title( '\n\n something between newlines \n\n' ) ).to_not match( /^\n\n/ )
+      str = '\n\n something between newlines \n\n' 
+      expect( @scraper.process_title( str ) ).to_not match( /^\n\n/ )
     end
   end
 
